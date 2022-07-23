b8585c300f3e890d82e370a52382aa82ac8aee8d
VHarasymiv <41122474+VHarasymiv@users.noreply.github.com>
2022-07-24 01:39:19 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+(function() {
+  const acc = document.getElementsByClassName("acc-btn");
+
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+    });
+  }
+})();
